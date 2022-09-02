9c9866ed148ecc74a70131b7e3f25707d14f354e
dexter68555 <46341564+dexter68555@users.noreply.github.com>
2022-09-02 21:46:54 +0800

    Update SimpleStorage.sol

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,6 +4,7 @@ pragma solidity >= 0.5.0 < 0.9.0;
 
 contract SimpleStorage{
 
+    //variable declaration
     uint256 bestNumber = 5;
     bool bestBool = true;
     string bestString = "test123";
@@ -11,6 +12,7 @@ contract SimpleStorage{
     address bestAddress = 0x5555555555555555555555555555555555555555;
     bytes32 bestByte = "dog";
 
+    //struct data People
     struct People{
 
         uint256 bestNumber;
@@ -18,23 +20,24 @@ contract SimpleStorage{
 
     }
 
-    // People public person = People({bestNumber: 55, name: "Bruce Wayne"});
-
     People [] public people;
     mapping (string => uint256) public nameToBestNumber;
 
+    //store function to store number
     function store(uint256 _bestNumber) public {
 
         bestNumber = _bestNumber;
 
     }
 
+    //getNumber function to get number
     function getNumber() public view returns(uint256){
 
         return bestNumber;
 
     }
 
+    //addPerson function to add new person with number
     function addPerson(string memory _name, uint256 _bestNumber) public {
 
         people.push(People(_bestNumber, _name));
